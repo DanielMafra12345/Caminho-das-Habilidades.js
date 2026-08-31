76be05fc154c36e2d3ad85d7fa7e9fd04c56db89
DanielMafra12345 <daniel.mafra.pinto@escola.pr.gov.br>
2026-08-31 08:49:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const desafios =[
+    "Resolver problemas de lógica",
+    "Aprender novas funções",
+    "Criar novas soluções",
+    "Aprender padrões",
+    "Criar uma invenção"
+];
+
+function iniciarDesafio() {
+    const nome=document.getElementById("name").value;
+}
+
+if (nome === "") {
+    alert("Digite seu nome para começar!");
+    return;
+}    
